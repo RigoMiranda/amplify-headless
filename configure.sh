b8956265b66ebba7fd66483ae8de39e593463934
Rigoberto Miranda <rygo.107@gmail.com>
2020-05-14 22:14:41 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -8,8 +8,8 @@ AWSCLOUDFORMATIONCONFIG="{\
 \"configLevel\":\"project\",\
 \"useProfile\":true,\
 \"profileName\":\"default\",\
-\"accessKeyId\":\"${AWS_ACCESS_KEY_ID}\",\
-\"secretAccessKey\":\"${AWS_SECRET_ACCESS_KEY}\",\
+\"accessKeyId\":\"$AWS_ACCESS_KEY_ID\",\
+\"secretAccessKey\":\"$AWS_SECRET_ACCESS_KEY\",\
 \"region\":\"us-east-1\"\
 }"
 AMPLIFY="{\
