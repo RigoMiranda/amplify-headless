e63b2898875fba185d4a943b76ba4f65c6b0773b
Rigoberto Miranda <rmiranda.s@me.com>
2020-05-14 22:08:08 -0400

    Initial Commit

---
+++ configure.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+IFS='|'
+
+echo "'${AWS_ACCESS_KEY_ID}' '${AWS_SECRET_ACCESS_KEY}'"
+
+AWSCLOUDFORMATIONCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":true,\
+\"profileName\":\"default\",\
+\"accessKeyId\":\"${AWS_ACCESS_KEY_ID}\",\
+\"secretAccessKey\":\"${AWS_SECRET_ACCESS_KEY}\",\
+\"region\":\"us-east-1\"\
+}"
+AMPLIFY="{\
+\"projectName\":\"headlessTemplate\",\
+\"envName\":\"dev\",\
+\"defaultEditor\":\"code\"\
+}"
+FRONTEND="{\
+\"frontend\":\"ios\"
+}"
+PROVIDERS="{\
+\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+}"
+CATEGORIES="{\
+\"auth\":$AUTHCONFIG\
+}"
+
+amplify init \
+--amplify $AMPLIFY \
+--frontend $FRONTEND \
+--providers $PROVIDERS \
+--yes
