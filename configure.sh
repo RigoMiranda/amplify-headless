c4d3a2b27cf74c3a4d418dbe248c4394a9849e2a
Rigoberto Miranda <rygo.107@gmail.com>
2020-05-14 22:16:13 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 set -e
 IFS='|'
 
-echo "'${AWS_ACCESS_KEY_ID}' '${AWS_SECRET_ACCESS_KEY}'"
+echo "'$AWS_ACCESS_KEY_ID' '$AWS_SECRET_ACCESS_KEY'"
 
 AWSCLOUDFORMATIONCONFIG="{\
 \"configLevel\":\"project\",\
